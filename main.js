0b455b55f7c9bdf7742b0a0d35d5c9eaf3d995c8
Marjorie-Pereira <marjorie300904@gmail.com>
2024-07-25 20:41:55 -0300

    main files pushed

---
+++ main.js
@@ -0,0 +1,23 @@
+const termo = 'TEMPO';
+const linhaSelecionada = document.querySelectorAll('#linha-selecionada');
+const tentativas = document.querySelector('.tentativas');
+
+// coletando input e exibindo na tela
+
+
+
+// verificando input do usuário
+// for(let i = 0; i < linhaSelecionada.length; i++) {
+//     if(termo.includes(linhaSelecionada[i].innerHTML) ) {
+        
+//         // comparando o indice da letra encontrada no termo com o indice da letra no elemento 
+//         if(termo.indexOf(linhaSelecionada[i].innerHTML) == i) {
+//             linhaSelecionada[i].id = 'letra-certa'; 
+//         } else {
+//             linhaSelecionada[i].id = 'posicao-errada';
+//         }
+//     } else {
+//         linhaSelecionada[i].id = 'letra-errada';
+//     }
+// }
+console.log(tentativas.children);
